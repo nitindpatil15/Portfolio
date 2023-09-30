4b8589f42246f3c911f826155eae8618ac85a229
Nitin Dagadu Patil <113434416+nitindpatil15@users.noreply.github.com>
2023-09-30 22:07:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element',{
+    strings: ['Web Developer', 'Web Designer'],
+    typeSpeed:50,
+});
+
+let a = document.getElementsByClassName('left')
+a.onclick=location('#Home')
